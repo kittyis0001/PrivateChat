279b968ef1ca586224e1d0c5b5f10602df611e4a
Md Shihab <kittyis0001@gmail.com>
2026-08-21 10:25:54 +0600

    Update Firebase database

---
+++ app/build/generated/source/buildConfig/debug/com/privatechat/app/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.privatechat.app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.privatechat.app";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String FIREBASE_DATABASE_URL = "https://private-chat-318a6-default-rtdb.asia-southeast1.firebasedatabase.app";
+}
